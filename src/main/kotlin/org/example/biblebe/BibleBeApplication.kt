dd56e4f212367417ca702780927d5e416d65cc24
kimsee8200 <ttungseui11@gmail.com>
2025-03-17 16:14:26 +0900

    first setting

---
+++ src/main/kotlin/org/example/biblebe/BibleBeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.biblebe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BibleBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BibleBeApplication>(*args)
+}
