1f65863a0264a12b53f8d09e53a7404ad8714aaf
오송주 <dhthdwn7920@gmail.com>
2025-04-30 15:54:10 +0900

    feat: update todo 기능개발

--- src/main/kotlin/org/example/biblebe/domain/todo/controller/TodoController.kt
+++ src/main/kotlin/org/example/biblebe/domain/todo/controller/TodoController.kt
@@ -2,8 +2,10 @@ package org.example.biblebe.domain.todo.controller
 
 import jakarta.validation.Valid
 import org.example.biblebe.domain.todo.dto.request.CreateTodoRequestDto
+import org.example.biblebe.domain.todo.dto.request.UpdateTodoRequestDto
 import org.example.biblebe.domain.todo.usecase.CreateTodoUseCase
 import org.example.biblebe.domain.todo.usecase.DeleteTodoUseCase
+import org.example.biblebe.domain.todo.usecase.UpdateTodoUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -12,7 +14,8 @@ import java.util.*
 @RequestMapping("/calender")
 class TodoController (
     private val createTodoUseCase: CreateTodoUseCase,
-    private val deleteTodoUseCase: DeleteTodoUseCase
+    private val deleteTodoUseCase: DeleteTodoUseCase,
+    private val updateTodoUseCase: UpdateTodoUseCase
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -25,5 +28,14 @@ class TodoController (
     @DeleteMapping("/{todoId}")
     fun deleteTodo(@PathVariable todoId: UUID) {
         deleteTodoUseCase.execute(todoId)
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/{todoId}")
+    fun updateTodo(
+        @PathVariable todoId: UUID,
+        @Valid @RequestBody request: UpdateTodoRequestDto
+    ) {
+        updateTodoUseCase.execute(todoId, request)
     }
 }
